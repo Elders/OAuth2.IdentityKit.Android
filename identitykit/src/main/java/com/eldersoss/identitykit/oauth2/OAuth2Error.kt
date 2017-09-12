f3ea9117000b055f51d4ae42b0dfd9881f7700e9
Ivan Vatov <ivan.vatov@eldersoss.com>
2017-09-12 12:25:05 +0300

    Invalid token response error message

--- identitykit/src/main/java/com/eldersoss/identitykit/oauth2/OAuth2Error.kt
+++ identitykit/src/main/java/com/eldersoss/identitykit/oauth2/OAuth2Error.kt
@@ -28,6 +28,7 @@ enum class OAuth2Error(val errorMessage: String?) : Error{
     unauthorized_client("unauthorized_client"),
     unsupported_grant_type("unsupported_grant_type"),
     invalid_scope("invalid_scope"),
+    invalid_token_response("invalid_token_response"),
     unknown(null);
 
     override fun getMessage(): String{
@@ -39,6 +40,7 @@ enum class OAuth2Error(val errorMessage: String?) : Error{
             "unauthorized_client" -> "The authenticated client is not authorized to use this authorization grant type"
             "unsupported_grant_type" -> "The authorization grant type is not supported by the authorization server"
             "invalid_scope" -> "The requested scope is invalid"
+            "invalid_token_response" -> "The received access token response is not valid"
             else -> "Unknown error"
         }
     }
